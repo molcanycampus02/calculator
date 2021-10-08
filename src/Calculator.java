0f0c79e9228a5c9b58a8df818df85e5466a68f95
KarinM <s44787@edu.campus02.at>
2021-10-08 19:58:48 +0200

    Variablen deklariert

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,15 @@
 public class Calculator {
     public static void main(String[] args) {
-
+    int a = 12;
+        System.out.println(a);
+    double b = 2.5;
+    boolean c = false;
+    char d = 'E';
+    String e = "abc";
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
     }
 }
+
